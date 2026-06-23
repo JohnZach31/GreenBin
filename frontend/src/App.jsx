621a91b3a1bd592eaac5bec574baf1148d4ec235
Johnathan Zachevsky <johnzachws@gmail.com>
2026-06-23 23:11:05 +0300

    Enviorment Variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
-const API_URL = 'http://192.168.1.139:3000/api/classify'
+const API_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:3000/api/classify'
 
 function App() {
   const videoRef = useRef(null)
